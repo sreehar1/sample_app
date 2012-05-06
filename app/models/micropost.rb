81d1d6b442a3aab8ec0b159fc0c05b1697b94df4
sreehar1 <kasagundi@gmail.com>
2012-05-06 12:22:27 -0400

    Made a working feed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,6 +9,22 @@ class Micropost < ActiveRecord::Base
 
 	default_scope :order => 'microposts.created_at DESC'
 
+	scope :from_users_followed_by, lambda { |user| followed_by(user) }
+
+	private
+	# Return an SQL condition for users followed by the given user.
+	# We include the user's own id as well.
+	def self.followed_by(user)
+		following_ids = %(SELECT followed_id FROM relationships
+		WHERE follower_id = :user_id)
+
+		where("user_id IN (#{following_ids}) OR user_id = :user_id",
+		{ :user_id => user })
+	end
+
+	def self.from_users_followed_by(user)
+		where(:user_id => user.following.push(user))
+	end
 end
 
 # == Schema Information
